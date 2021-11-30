501347203c8a0d65baa577d21da690ec7025d813
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-11-30 22:11:06 +0300

    Add menu scene

--- Assets/Scripts/CloudMove.cs
+++ Assets/Scripts/CloudMove.cs
@@ -7,7 +7,7 @@ public class CloudMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Destroy(gameObject, 10); 
+       Destroy(gameObject, 10);  
     }
 
 
